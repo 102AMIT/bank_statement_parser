b2be1aea932dd577e4eb0a3241f77be135c04418
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-12-07 00:53:35 +0530

    basic server setup complete using express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const PORT=8000;
+const app=express();
+
+
+
+
+app.listen(PORT,function(err){
+    if(err){
+        console.log(`Error in Server Running `);
+    }
+    console.log(`Server Running On PORT : ${PORT}`);
+    
+})
